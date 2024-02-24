bdf0b1ca3600520eed5cbd8aa986292023b5f6c5
Jose Mauricio Piron <contato.mauriciopiron@gmail.com>
2024-02-23 22:26:19 -0300

    refactor: Ajuste no codigo

--- colecoes-part-2/src/br/com/mauriciopiron/ClassificaSexo.java
+++ colecoes-part-2/src/br/com/mauriciopiron/ClassificaSexo.java
@@ -10,28 +10,29 @@ public class ClassificaSexo {
         Scanner sc = new Scanner(System.in);
         System.out.print("Quantos nomes desejar informar? ");
 
-        int quantity = sc.nextInt();
+        int count = sc.nextInt();
         sc.nextLine();
 
         List<String> homens = new ArrayList<>();
         List<String> mulheres = new ArrayList<>();
 
-        for (int i = 0; i < quantity; i++) {
+        for (int i = 0; i < count; i++) {
             System.out.print("Informe o " + (i+1) + "° nome: ");
             String name = sc.nextLine();
             System.out.print("Informe M - Masculino ou F - Feminino ");
-            String gender = sc.nextLine();
+            String sexo = sc.nextLine();
 
-            if ("F".equals(gender.toUpperCase())) mulheres.add(name);
-            else if ("M".equals(gender.toUpperCase())) homens.add(name);
+            if ("F".equals(sexo.toUpperCase())) mulheres.add(name);
+            else if ("M".equals(sexo.toUpperCase())) homens.add(name);
             else System.out.println("Não identifiquei o sexo informado, esta pessoa não será registrada");
         }
 
         System.out.println("Lista dos homens:");
-        for (String male : homens) System.out.println(" - " + male);
+        System.out.println(" - " + homens);
 
         System.out.println("Lista das mulheres:");
-        for (String female : mulheres) System.out.println(" - " + female);
+        System.out.println(" - " + mulheres);
+
         sc.close();
     }
 }
